4a0cee454884cea7c3bcc111aa7ff9d573657a0d
viallon <viallonv@iarc.fr>
2020-02-17 13:46:06 +0100

    correction of a few typos (in particular for Ridge-AdaLasso; see Lines 25 in AdapGlmnet-NestedCV)

--- AdapGlmnet-NestedCv.R
+++ AdapGlmnet-NestedCv.R
@@ -1,32 +1,36 @@
 # adap: TRUE  --> lasso adap 
 #       FALSE --> lasso std
 
-# weights.alpha: 1 --> initial estimator : lasso --> onestep
+# alpha.weights: 1 --> initial estimator : lasso --> onestep
 #                0 --> initial estimator : ridge 
 #               99 --> initial estimator : mco   
 
 
 
-adap.glmnet <- function(adap=TRUE,weights.alpha=1,eps=0,nested.foldid=NULL,x,y,family="gaussian",alpha =1,penalty.factor=rep(1,ncol(x)), intercept = FALSE){
+adap.glmnet <- function(x, y, adap=TRUE, alpha.weights=1, eps=0, nested.foldid=NULL, nfolds=10, family="gaussian", penalty.factor=rep(1,ncol(x))){
   
-  if (adap==FALSE) {
-    modstep1=nested.cvglmnet(nested.cv=adap,nested.foldid=nested.foldid,x=x,y=y,family=family,alpha =alpha,penalty.factor=penalty.factor, intercept = intercept)
+  if (adap==FALSE) {# standard Lasso
+    modstep1=nested.cvglmnet(nested.cv=adap,nested.foldid=nested.foldid,x=x,y=y,family=family,penalty.factor=penalty.factor)
     return(modstep1) 
-  }else{
+  }else{# Adaptive Lasso
     
-    if(weights.alpha==99){
-      #add intercept option
+  if(alpha.weights==99){ # OLS-AdaLasso
       modstep1 = glm(y~x,family=family)
       Weights=1/(abs(modstep1$coefficients[-1])+eps)
       
-      modstep2 = nested.cvglmnet(penalty.factor=Weights,nested.cv=adap,weights.alpha=weights.alpha,eps=eps,nested.foldid=nested.foldid,x=x,y=y,family=family,alpha=alpha, intercept = intercept)
+      # Selection of the parameter using (Nested and not std) CV
+      modstep2 = nested.cvglmnet(penalty.factor=Weights,nested.cv=adap,alpha.weights=alpha.weights,eps=eps,nested.foldid=nested.foldid,x=x,y=y,family=family)
       
-    }else{
+    }else{# weights derived from glmnet, with alpha set to alpha.weights (1: Lasso-> one-step Lasso; 0: ridge -> Ridge-AdaLasso)
       
-      modstep1 = nested.cvglmnet(penalty.factor=rep(1,ncol(x)),nested.cv=!adap,nested.foldid=nested.foldid,x=x,y=y,family=family,alpha=alpha, intercept = FALSE)
-      Weights=1/(abs(modstep1$glmnet.fit$beta[,which(modstep1$glmnet.fit$lambda==modstep1$lambda.min)])+eps)
+      #modstep1 = nested.cvglmnet(penalty.factor=rep(1,ncol(x)), nested.cv=!adap, nested.foldid=nested.foldid,x=x,y=y,family=family)
+      #Weights=1/(abs(modstep1$glmnet.fit$beta[,which(modstep1$glmnet.fit$lambda==modstep1$lambda.min)])+eps)
+      modstep1 = cv.glmnet(x=x,y=y,family=family, alpha = alpha.weights, penalty.factor=rep(1,ncol(x)), nfold=ifelse(is.null(nested.foldid), nfolds, max(nested.foldid)))
+      Weights=1/(abs( coef(modstep1, s= modstep1$lambda.min)[-1]) + eps)
       
-      modstep2 = nested.cvglmnet(penalty.factor=Weights,nested.cv=adap,weights.alpha=weights.alpha,eps=eps,nested.foldid=nested.foldid,x=x,y=y,family=family,alpha=alpha, intercept = intercept)
+      
+      
+      modstep2 = nested.cvglmnet(penalty.factor=Weights,nested.cv=adap,alpha.weights=alpha.weights,eps=eps,nested.foldid=nested.foldid,x=x,y=y,family=family)
     }
     return(modstep2) 
   }
@@ -39,59 +43,58 @@ adap.glmnet <- function(adap=TRUE,weights.alpha=1,eps=0,nested.foldid=NULL,x,y,f
 
 
 
-nested.cvglmnet <- function(nested.cv=FALSE,weights.alpha=1,eps=0,nested.foldid=NULL,x,y,family,alpha =1,penalty.factor=rep(1,ncol(x)),intercept=TRUE,nfolds=10){
+nested.cvglmnet <- function(nested.cv=FALSE, alpha.weights=1, eps=0, nested.foldid=NULL, x, y, family="gaussian", penalty.factor=rep(1,ncol(x)), nfolds=10){
+  
+  # creation of the fold id for the cross-validation
+  if(is.null(nested.foldid)){nested.foldid=sample(rep_len(1:nfolds,length(y)))}
+  
+  # first CV (the only one that is usually applied)
+  mod = cv.glmnet(x=x, y=y, family=family, penalty.factor=penalty.factor, foldid=nested.foldid, alpha = 1)
   
   
-  #Standard
-  #if(is.null(nested.foldid)){nested.foldid=rep_len(1:nfolds,length(y))[sample(length(y))]}
-  
-  #For Simulations
-  if(is.null(nested.foldid)){nested.foldid=rep_len(1:nfolds,length(y))}
-  
-  
-  mod = cv.glmnet(x=x,y=y,family=family,alpha = alpha,penalty.factor=penalty.factor,intercept=intercept,foldid=nested.foldid)
-  
-  if (nested.cv==TRUE) {
-    
-    
+  if (nested.cv==TRUE) {# second, "nested"-CV
+
     nested.cverror=matrix(NA,nrow = max(nested.foldid),ncol = length(mod$lambda))
     
-    
-    
     for(k in 1:max(nested.foldid)) {
-      
+      # we first re-compute the weights on each train sample
+      # using either OLS fit (OLS-AdaLasso) or glmnet fit (one-step Lasso or Ridge-AdaLasso)
+
       nested.fold <- which(nested.foldid == k)
       
-      if(weights.alpha==99){
+      if(alpha.weights==99){# OLS fit for the weights
         
         mod.fold = glm(y[-nested.fold]~x[-nested.fold,],family=family)
         Weights=1/(abs(mod.fold$coefficients[-1])+eps)
         
-      }else{
+      }else{ # glmnet fit for the weights, with alpha set to alpha.weights
         
-        mod.fold = cv.glmnet(x=x[-nested.fold,],y=y[-nested.fold],family=family,alpha = weights.alpha,penalty.factor=rep(1,ncol(x)),intercept=intercept)
-        Weights=1/(abs(mod.fold$glmnet.fit$beta[,which(mod.fold$glmnet.fit$lambda==mod.fold$lambda.min)])+eps)
+        mod.fold = cv.glmnet(x=x[-nested.fold,], y=y[-nested.fold], family=family, alpha = alpha.weights, penalty.factor=rep(1,ncol(x)))
+        #Weights=1/(abs(mod.fold$glmnet.fit$beta[,which(mod.fold$glmnet.fit$lambda==mod.fold$lambda.min)])+eps)
+        Weights= 1/(abs( coef(mod.fold, s= mod.fold$lambda.min)[-1]) + eps)
+        
       }
       
-      modad.fold =glmnet(x=x[-nested.fold,],y=y[-nested.fold],family=family,lambda=mod$lambda,penalty.factor = Weights,alpha = alpha,intercept=intercept)
+      # compute the adaptive Lasso on the train sample, with penalty.factor set to Weights (computed on the train sample), 
+      # and the lambda sequence computed from the whole sample
+      modad.fold =glmnet(x=x[-nested.fold,],y=y[-nested.fold],family=family, lambda=mod$lambda, penalty.factor = Weights,alpha = 1)
       
-      #Missing: generalize the cv.error to all family (logistic, multinomial,...)
+      # And now compute the CV prediction error on the test sample
+      #  => to be done: generalize the cv.error to all family (logistic, multinomial,...)
       for (l in 1:length(mod$lambda)) {
         nested.cverror[k,l]= (1/length(y[nested.fold]))*sum((y[nested.fold]-(x[nested.fold,]%*%modad.fold$beta[,l]+modad.fold$a0[l]))^2)
       }
       
     }
     
-    cvm=apply(nested.cverror, 2, mean)
-    cvsd=apply(nested.cverror, 2, sd)/sqrt(max(nested.foldid))
-    cvup=cvm+cvsd
-    cvlo=cvm-cvsd
-    lambda.min=mod$lambda[which.min(cvm)]
-    
-    under.cvup1se=which(cvm<=cvup[which.min(cvm)]*1)
-    min.nzero1se=under.cvup1se[which.min(mod$nzero[under.cvup1se])]
-    
-    lambda.1se=mod$lambda[min.nzero1se]
+    cvm  = apply(nested.cverror, 2, mean)
+    cvsd = apply(nested.cverror, 2, sd)/sqrt(max(nested.foldid))
+    cvup = cvm + cvsd
+    cvlo = cvm - cvsd
+    lambda.min    = mod$lambda[which.min(cvm)]
+    under.cvup1se = which(cvm<=cvup[which.min(cvm)]*1)
+    min.nzero1se  = under.cvup1se[which.min(mod$nzero[under.cvup1se])]
+    lambda.1se    = mod$lambda[min.nzero1se]
     
     addto.glmnet=c("cvm","cvsd","cvup","cvlo","lambda.min","lambda.1se")
     
--- Simulations.R
+++ Simulations.R
@@ -1,29 +1,33 @@
 library(Matrix)
 library(glmnet)
 library(parallel)
-path="C://Users/ballout/Desktop/CDESS/PAPIER-3/Codes/N/Codes/"
+library(here)
 
-source(paste0(path,"AdapGlmnet-NestedCv.R"))
+#path="C://Users/ballout/Desktop/CDESS/PAPIER-3/Codes/N/Codes/"
+
+source("AdapGlmnet-NestedCv.R")
 
 #settings
 
-vsim=1:100
-vp=c(100,500,1000)
-vsignal=c(0.4,0.8,1.2,2.4)
-vs0=c(0.05,0.1,0.25)
-eps=1e-4
-ntrain=c(1000,2000)
-ntest=ntrain*10
+vsim    = 1:100
+vp      = c(100,500,1000)
+vsignal = c(0.25,0.5,1,2)
+vs0     = c(10, 50)
+eps     = 1e-4
+ntrain  = c(1000,2000)
+ntest   = 10000
 
 
 code.meth=rbind(c(FALSE,rep(TRUE,3)),c(NA,1,0,99))
 colnames(code.meth)=c("Lasso","Onestep","Adlasso.ridge","Adlasso.mco")
-rownames(code.meth)=c("adap","weights.alpha")
+rownames(code.meth)=c("adap","alpha.weights")
 
 for(n in 1:length(ntrain)){
   for(p in vp){
     for(s0 in vs0){
       for(signal in vsignal){
+        
+        # n= 1; p=100; s0=10; signal = 0.5
         funparall<- function(sim){
           res=list()
           
@@ -31,7 +35,7 @@ for(n in 1:length(ntrain)){
           x=matrix(rnorm(p*ntot),ncol = p)
           
           beta=rep(0,p)
-          beta[sample(1:p,s0*p)]= (2*rbinom(n = s0*p,size = 1,prob = 0.5)-1)*signal
+          beta[sample(1:p,s0)]= (2*rbinom(n = s0,size = 1,prob = 0.5)-1)*signal
 
           y=x%*%beta+rnorm(ntot,0,1)
           
@@ -41,22 +45,33 @@ for(n in 1:length(ntrain)){
           xtest=x[(ntrain[n]+1):ntot,]
           ytest=y[(ntrain[n]+1):ntot]
           
-          if(p>=(ntrain[n]/2)){vweights.alpha=c(1,0)}else{vweights.alpha=c(1,0,99)}
+          if(p>=(ntrain[n]/2)){valpha.weights=c(1,0)}else{valpha.weights=c(1,0,99)}
           
           
          for (adap in c(FALSE,TRUE)) {
             if (adap==TRUE){
-            for (weights.alpha in vweights.alpha) {
+            for (alpha.weights in valpha.weights) {
             
-             mod=adap.glmnet(adap=adap,weights.alpha=weights.alpha,eps=0,x=xtrain,y=ytrain,family="gaussian",alpha =1,intercept = FALSE)
+             mod=adap.glmnet(adap=adap,alpha.weights=alpha.weights,eps=eps,x=xtrain,y=ytrain,family="gaussian")#,alpha =1, intercept = TRUE)
              
-             eval(parse(text=paste0("res$",names(which(code.meth[1,]==adap & code.meth[2,]==weights.alpha)),"=mod")))}
+             eval(parse(text=paste0("res$",names(which(code.meth[1,]==adap & code.meth[2,]==alpha.weights)),"=mod")))}
               }else{
               
-              mod=adap.glmnet(adap=adap,x=xtrain,y=ytrain,family="gaussian",alpha =1,intercept = FALSE)
+              mod=adap.glmnet(adap=adap,x=xtrain,y=ytrain,family="gaussian")#,alpha =1, intercept = TRUE)
               
               res$Lasso=mod}
          }
+          
+          plot(res$Onestep$lambda[30:100], res$Onestep$cvm[30:100], log="x")
+          plot(res$Onestep$lambda[30:100], res$Onestep$nested.cvm[30:100], log="x")  
+          
+          plot(res$Onestep$lambda, res$Onestep$cvm, log="x")
+          plot(res$Onestep$lambda, res$Onestep$nested.cvm, log="x")  
+          
+          
+          cbind(beta, res$Lasso$glmnet.fit$beta[, which(res$Lasso$lambda == res$Lasso$lambda.min)], res$Onestep$glmnet.fit$beta[, which(res$Onestep$lambda == res$Onestep$lambda.min)], 
+                res$Onestep$glmnet.fit$beta[, which(res$Onestep$lambda == res$Onestep$nested.lambda.min)])
+          
           
         res$train$x=xtrain
         res$train$y=ytrain
