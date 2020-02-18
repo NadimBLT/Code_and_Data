d3bdf2a4297a7d7f8e05935b80b7cfd77537680a
NadimBLT <44828296+NadimBLT@users.noreply.github.com>
2020-02-19 00:27:42 +0100

    Simulations+Evaluations

--- Simulations.R
+++ Simulations.R
@@ -3,33 +3,52 @@ library(glmnet)
 library(parallel)
 library(here)
 
-#path="C://Users/ballout/Desktop/CDESS/PAPIER-3/Codes/N/Codes/"
+setwd("~")
 
 source("AdapGlmnet-NestedCv.R")
 
+
+Eval <- function(beta.star,beta.hat,intercept.hat,x,y){
+  
+  prediction.error= mean((y - (x%*%beta.hat + intercept.hat))^2)
+  
+  beta.star[which(beta.star<0)]=-1
+  beta.star[which(beta.star>0)]=1
+  beta.hat[which(beta.hat<0)]=-1
+  beta.hat[which(beta.hat>0)]=1
+  
+  accuracy=length(which(beta.hat==beta.star))/length(beta.star)
+  return(c(accuracy,prediction.error))
+}
+
 #settings
 
-vsim    = 1:100
+vsim    = 1:5
 vp      = c(100,500,1000)
 vsignal = c(0.25,0.5,1,2)
 vs0     = c(10, 50)
 eps     = 1e-4
 ntrain  = c(1000,2000)
-ntest   = 10000
+ntest   = ntrain*10
 
 
 code.meth=rbind(c(FALSE,rep(TRUE,3)),c(NA,1,0,99))
-colnames(code.meth)=c("Lasso","Onestep","Adlasso.ridge","Adlasso.mco")
+colnames(code.meth)=c("Lasso","One.step.Lasso","Ridge.AdaLasso","OLS.AdaLasso")
 rownames(code.meth)=c("adap","alpha.weights")
+
+TableALL=NULL
 
 for(n in 1:length(ntrain)){
   for(p in vp){
     for(s0 in vs0){
       for(signal in vsignal){
-        
-        # n= 1; p=100; s0=10; signal = 0.5
+    
         funparall<- function(sim){
-          res=list()
+          # to save all
+          ## res=list()
+          
+          evaluation <- matrix(ncol = 4,nrow = 0)
+          colnames(evaluation)=c("Accuracy","PredError","Method","Type")
           
           ntot=ntrain[n]+ntest[n]
           x=matrix(rnorm(p*ntot),ncol = p)
@@ -54,45 +73,64 @@ for(n in 1:length(ntrain)){
             
              mod=adap.glmnet(adap=adap,alpha.weights=alpha.weights,eps=eps,x=xtrain,y=ytrain,family="gaussian")#,alpha =1, intercept = TRUE)
              
-             eval(parse(text=paste0("res$",names(which(code.meth[1,]==adap & code.meth[2,]==alpha.weights)),"=mod")))}
+             # to save all :
+             ## eval(parse(text=paste0("res$",names(which(code.meth[1,]==adap & code.meth[2,]==alpha.weights)),"=mod")))
+            
+             beta.hat1=coef(mod, s= mod$lambda.min)
+             beta.hat2=coef(mod, s= mod$nested.lambda.min)
+             
+             evaluation <- rbind(evaluation,
+                                 c(Eval(beta.star = beta,beta.hat = beta.hat1[-1],intercept.hat = beta.hat1[1],y=ytest,x=xtest),names(which(code.meth[1,]==adap & code.meth[2,]==alpha.weights)),"CV"),
+                                 c(Eval(beta.star = beta,beta.hat = beta.hat2[-1],intercept.hat = beta.hat2[1],y=ytest,x=xtest),names(which(code.meth[1,]==adap & code.meth[2,]==alpha.weights)),"nestedCV"))
+              
+              }
+              
               }else{
               
               mod=adap.glmnet(adap=adap,x=xtrain,y=ytrain,family="gaussian")#,alpha =1, intercept = TRUE)
               
-              res$Lasso=mod}
+              # to save all
+              ## res$Lasso=mod
+              
+              beta.hat=coef(mod, s= mod$lambda.min)
+              evaluation <- rbind(evaluation,c(Eval(beta.star = beta,beta.hat = beta.hat[-1],intercept.hat = beta.hat[1],y=ytest,x=xtest),"Lasso","CV"))
+              
+              }
          }
           
-          # plot(res$Onestep$lambda[30:100], res$Onestep$cvm[30:100], log="x")
-          # plot(res$Onestep$lambda[30:100], res$Onestep$nested.cvm[30:100], log="x")  
-          # 
-          # plot(res$Onestep$lambda, res$Onestep$cvm, log="x")
-          # plot(res$Onestep$lambda, res$Onestep$nested.cvm, log="x")  
-          # 
-          # 
-          # cbind(beta, res$Lasso$glmnet.fit$beta[, which(res$Lasso$lambda == res$Lasso$lambda.min)], res$Onestep$glmnet.fit$beta[, which(res$Onestep$lambda == res$Onestep$lambda.min)], 
-          #       res$Onestep$glmnet.fit$beta[, which(res$Onestep$lambda == res$Onestep$nested.lambda.min)])
-          # 
-          
-        res$train$x=xtrain
-        res$train$y=ytrain
-        
-        res$test$x=xtest
-        res$test$y=ytest
-        
-        res$beta.star=beta
-        
-        # we can do the evaluations here to avoid saving large files
-        # ex: ntrain = 1000 + p = 20  ~ save = 1.5 mb 
-        #                   + p = 100 ~ save = 8.0 mb
-        #                   + p = 500 ~ save = 40.0 mb
+        evaluation <- data.frame(evaluation,p=p,s0=s0,signal=signal,n=ntrain[n],sim=sim)
          
-        save(res,file = paste0(path,"res/ntrain-",ntrain[n],"-ntest-",ntest[n],"-nvar-",p,"-sparsity-",s0,"-signal-",signal,"sim-",sim,".RData"))
+        # to save all : 
+        
+        ##res$train$x=xtrain
+        ##res$train$y=ytrain
+        
+        ##res$test$x=xtest
+        ##res$test$y=ytest
+        
+        ##res$beta.star=beta
+        
+        ## res$evaluation <- evaluation
        
+        ## save(res,file = paste0(path,"res/ntrain-",ntrain[n],"-ntest-",ntest[n],"-nvar-",p,"-sparsity-",s0,"-signal-",signal,"sim-",sim,".RData"))
+        
+        return(evaluation)
         
         }
-        mclapply(vsim,FUN = funparall,mc.cores = 10)
         
+       Res=mclapply(vsim,FUN = funparall,mc.cores = 10)
+       
+       Table <- NULL
+       for(i in 1:length(vsim)){Table=rbind(Table,Res[[i]])}
+        
+       #back up: record a table for each combination of p*s0*n*signal
+        save(Table,file = paste0("ntrain-",ntrain[n],"-ntest-",ntest[n],"-nvar-",p,"-sparsity-",s0,"-signal-",signal,"AllSim.RData"))
+       
+       TableALL=rbind(TableALL,Table)
       }
     }
   }
 }
+
+save(TableALL,file = "TableALL.Rdata")
+
