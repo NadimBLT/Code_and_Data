ad79dc479fbd70e5ecc239c20ed8c2a14b7bde7d
NadimBLT <44828296+NadimBLT@users.noreply.github.com>
2022-09-21 13:15:46 +0200

    Update simulation.R
    Tables and Figures sections have been added

--- simulation/simulation.R
+++ simulation/simulation.R
@@ -8,7 +8,8 @@
 ## -----------------------------------------------------------------------------
 ## Sections in the article: main(section 2.3 and 4) + suppmat
 ## -----------------------------------------------------------------------------
-## Required Packages: glmnet, Matrix, parallel, here, MASS
+## Required Packages: glmnet, Matrix, parallel, here, MASS, dplyr, ggplot2,
+##                    openxlsx
 ## -----------------------------------------------------------------------------
 
 
@@ -22,6 +23,9 @@ library(glmnet)
 library(Matrix)
 library(parallel)
 library(MASS)
+library(dplyr)
+library(ggplot2)
+library(openxlsx)
 
 source("../functions/ada.lasso.nested.cv.R")
 source("../functions/ada.lasso.naive.cv.R")
@@ -243,7 +247,339 @@ if ( ! file.exists("intermediate_results/")) {
 saveRDS(res.table, file = "intermediate_results/res_table.rds")
 
 
+
+
+
+
+###################
+###   Tables    ###
+###################
+
+### Table 1
+# Averages of the computational times (in minutes)
+
+tab.table <- res.table
+
+tab.table$Runtime <- as.numeric(as.character(tab.table$Runtime))
+
+main.tab.1 <- tab.table %>% group_by(Method) %>% summarise(Av = mean(Runtime)) 
+
+
+# save Tables
+
+if ( ! file.exists("tables/") ) {
+  dir.create("tables")
+}
+
+write.xlsx(data.frame(main.tab.1), file = "tables/main_tab_1.xlsx",
+           sheetName = "main_tab_1", append = FALSE)
+
+
 ###################
 ###   Figures   ###
 ###################
+
+
+# formatting the results table
+
+fig.table <- rbind(data.frame(Value = res.table$sACC,
+                              Criterion = "sACC",
+                              res.table[, -c(1:5)]),
+                   data.frame(Value = res.table$Precision,
+                              Criterion = "Precision",
+                              res.table[, -c(1:5)]),
+                   data.frame(Value = res.table$Recall,
+                              Criterion = "Recall",
+                              res.table[, -c(1:5)]),
+                   data.frame(Value = res.table$Pred.Error,
+                              Criterion = "Pred Error",
+                              res.table[, -c(1:5)]))
+
+
+fig.table$Value <- as.numeric(as.character(fig.table$Value))
+fig.table$Criterion <- factor(fig.table$Criterion, levels = 
+                                c("sACC", "Precision", "Recall", "Pred Error"))
+
+fig.table$Method <- factor(fig.table$Method,
+                           levels = c("adalasso.ow.nai",
+                                      "adalasso.ow.nest",
+                                      "adalasso.rw.nai",
+                                      "adalasso.rw.nest",
+                                      "adalasso.lw.nai",
+                                      "adalasso.lw.nest",
+                                      "lasso"),
+                           labels = c("ols-ada lasso naive CV",
+                                      "ols-ada lasso nested CV",
+                                      "ridge-ada lasso naive CV",
+                                      "ridge-ada lasso nested CV",
+                                      "one-step lasso naive CV", 
+                                      "one-step lasso nested CV",
+                                      "lasso CV"))
+
+# functions to use in ggplot()
+
+Inf_95 <- function (x) {
+  return(mean(x) - 1.96 * sd(x) / sqrt(length(x)))
+}
+Sup_95 <- function (x) {
+  return(mean(x) + 1.96 * sd(x) / sqrt(length(x)))
+}
+
+# theme to use in ggplot()
+
+theme <- theme(
+  axis.text = element_text(size = 7),
+  plot.background = element_rect(fill = "white"),
+  strip.text.x = element_text(size = 12, face = "bold", color = "black"),
+  strip.text.y = element_text(size = 12, face = "bold", color = "black"),
+  text = element_text(size = 12,color = "black"),
+  legend.position = "bottom",
+  legend.title = element_blank(),
+  legend.key = element_rect(fill = "white"),
+  legend.background = element_rect(fill = "white"),
+  legend.text = element_text(size = 12),
+  panel.grid.minor = element_blank(),
+  panel.grid.major = element_line(colour = "grey70",size = 0.1),
+  panel.background = element_rect(fill = "white",color = "grey70", size = 0.1),
+  strip.background = element_rect(colour = "grey30", fill = "white",size = 0.1))
+
+col <- c("brown2", "chartreuse2", "blue")
+
+
+
+### Main Figure 2
+
+main.fig.2 <-  
+  ggplot(data = fig.table %>% filter(rho == 0.3,
+                                     n == 500,
+                                     Method %in% c("one-step lasso naive CV",
+                                                   "one-step lasso nested CV",
+                                                   "lasso CV")),
+         mapping = aes(signal, Value, color = Method, group = Method)) +
+  stat_summary(fun = "mean", geom = "line", size = 0.8, linetype = 1) +
+  stat_summary(fun = "mean", geom = "point", size = 2) +
+  stat_summary(fun = "Inf_95", geom = "line", alpha = 0.7, linetype = 2,
+               size = 0.3) +
+  stat_summary(fun = "Sup_95",geom = "line", alpha =  0.7, linetype = 2,
+               size = 0.3) +
+  facet_grid( Criterion ~ p, scales = "free",
+              labeller = labeller(Criterion = label_value, p = label_both)) +
+  scale_color_manual(values = col) +
+  scale_y_continuous(limits = function (b) {
+    return(c(ifelse(min(b) == 1, 0.9, min(b)), max(b)))
+  }) +
+  scale_x_continuous(breaks = signal.seq,
+                     expand = expansion(mult = c(0.1, 0.1))) + theme + 
+  xlab("Signal strength") + ylab(" ") + 
+  guides(color = guide_legend(override.aes = list(size = 5)))
+
+
+
+### Main Figure 3
+
+
+
+### Supp Figure 1
+
+supp.fig.1 <-  
+  ggplot(data = fig.table %>% filter(rho == 0.6,
+                                     p %in% c(100, 1000, 10000),
+                                     Criterion == "Pred Error",
+                                     Method %in% c("one-step lasso naive CV",
+                                                   "one-step lasso nested CV")),
+         mapping = aes(signal, Value, color = Method, group = Method)) +
+  stat_summary(fun = "mean", geom = "line", size = 0.8, linetype = 1) +
+  stat_summary(fun = "mean", geom = "point", size = 2) +
+  stat_summary(fun = "Inf_95", geom = "line", alpha = 0.7, linetype = 2,
+               size = 0.3) +
+  stat_summary(fun = "Sup_95",geom = "line", alpha =  0.7, linetype = 2,
+               size = 0.3) +
+  facet_grid( n + p0 ~ p, scales = "free",
+              labeller = labeller(n = label_both, p0 = label_both,
+                                  p = label_both)) +
+  scale_color_manual(values = col[1:2]) +
+  scale_y_continuous(limits = function (b) {
+    return(c(ifelse(min(b) == 1, 0.9, min(b)), max(b)))
+  }) +
+  scale_x_continuous(breaks = signal.seq,
+                     expand = expansion(mult = c(0.1, 0.1))) + theme + 
+  xlab("Signal strength") + ylab(" ") + 
+  guides(color = guide_legend(override.aes = list(size = 5)))
+
+
+
+### Supp Figure 2
+
+supp.fig.2 <-  
+  ggplot(data = fig.table %>% filter(rho == 0.6,
+                                     p %in% c(100, 1000, 10000),
+                                     Criterion == "sACC",
+                                     Method %in% c("one-step lasso naive CV",
+                                                   "one-step lasso nested CV")),
+         mapping = aes(signal, Value, color = Method, group = Method)) +
+  stat_summary(fun = "mean", geom = "line", size = 0.8, linetype = 1) +
+  stat_summary(fun = "mean", geom = "point", size = 2) +
+  stat_summary(fun = "Inf_95", geom = "line", alpha = 0.7, linetype = 2,
+               size = 0.3) +
+  stat_summary(fun = "Sup_95",geom = "line", alpha =  0.7, linetype = 2,
+               size = 0.3) +
+  facet_grid( n + p0 ~ p, scales = "free",
+              labeller = labeller(n = label_both, p0 = label_both,
+                                  p = label_both)) +
+  scale_color_manual(values = col[1:2]) +
+  scale_y_continuous(limits = function (b) {
+    return(c(ifelse(min(b) == 1, 0.9, min(b)), max(b)))
+  }) +
+  scale_x_continuous(breaks = signal.seq,
+                     expand = expansion(mult = c(0.1, 0.1))) + theme + 
+  xlab("Signal strength") + ylab(" ") + 
+  guides(color = guide_legend(override.aes = list(size = 5)))
+
+
+
+### Supp Figure 3
+
+supp.fig.3 <-  
+  ggplot(data = fig.table %>% filter(rho == 0.6,
+                                     p %in% c(100, 1000, 10000),
+                                     Criterion == "Precision",
+                                     Method %in% c("one-step lasso naive CV",
+                                                   "one-step lasso nested CV")),
+         mapping = aes(signal, Value, color = Method, group = Method)) +
+  stat_summary(fun = "mean", geom = "line", size = 0.8, linetype = 1) +
+  stat_summary(fun = "mean", geom = "point", size = 2) +
+  stat_summary(fun = "Inf_95", geom = "line", alpha = 0.7, linetype = 2,
+               size = 0.3) +
+  stat_summary(fun = "Sup_95",geom = "line", alpha =  0.7, linetype = 2,
+               size = 0.3) +
+  facet_grid( n + p0 ~ p, scales = "free",
+              labeller = labeller(n = label_both, p0 = label_both,
+                                  p = label_both)) +
+  scale_color_manual(values = col[1:2]) +
+  scale_y_continuous(limits = function (b) {
+    return(c(ifelse(min(b) == 1, 0.9, min(b)), max(b)))
+  }) +
+  scale_x_continuous(breaks = signal.seq,
+                     expand = expansion(mult = c(0.1, 0.1))) + theme + 
+  xlab("Signal strength") + ylab(" ") + 
+  guides(color = guide_legend(override.aes = list(size = 5)))
+
+
+
+### Supp Figure 4
+
+supp.fig.4 <-  
+  ggplot(data = fig.table %>% filter(rho == 0.6,
+                                     p %in% c(100, 1000, 10000),
+                                     Criterion == "Recall",
+                                     Method %in% c("one-step lasso naive CV",
+                                                   "one-step lasso nested CV")),
+         mapping = aes(signal, Value, color = Method, group = Method)) +
+  stat_summary(fun = "mean", geom = "line", size = 0.8, linetype = 1) +
+  stat_summary(fun = "mean", geom = "point", size = 2) +
+  stat_summary(fun = "Inf_95", geom = "line", alpha = 0.7, linetype = 2,
+               size = 0.3) +
+  stat_summary(fun = "Sup_95",geom = "line", alpha =  0.7, linetype = 2,
+               size = 0.3) +
+  facet_grid( n + p0 ~ p, scales = "free",
+              labeller = labeller(n = label_both, p0 = label_both,
+                                  p = label_both)) +
+  scale_color_manual(values = col[1:2]) +
+  scale_y_continuous(limits = function (b) {
+    return(c(ifelse(min(b) == 1, 0.9, min(b)), max(b)))
+  }) +
+  scale_x_continuous(breaks = signal.seq,
+                     expand = expansion(mult = c(0.1, 0.1))) + theme + 
+  xlab("Signal strength") + ylab(" ") + 
+  guides(color = guide_legend(override.aes = list(size = 5)))
+
+
+
+### Supp Figure 5
+
+supp.fig.5 <-  
+  ggplot(data = fig.table %>% filter(rho == 0.3,
+                                     n == 500,
+                                     Method %in% c("ridge-ada lasso naive CV",
+                                                   "ridge-ada lasso nested CV",
+                                                   "lasso CV")),
+         mapping = aes(signal, Value, color = Method, group = Method)) +
+  stat_summary(fun = "mean", geom = "line", size = 0.8, linetype = 1) +
+  stat_summary(fun = "mean", geom = "point", size = 2) +
+  stat_summary(fun = "Inf_95", geom = "line", alpha = 0.7, linetype = 2,
+               size = 0.3) +
+  stat_summary(fun = "Sup_95",geom = "line", alpha =  0.7, linetype = 2,
+               size = 0.3) +
+  facet_grid( Criterion ~ p, scales = "free",
+              labeller = labeller(Criterion = label_value, p = label_both)) +
+  scale_color_manual(values = col) +
+  scale_y_continuous(limits = function (b) {
+    return(c(ifelse(min(b) == 1, 0.9, min(b)), max(b)))
+  }) +
+  scale_x_continuous(breaks = signal.seq,
+                     expand = expansion(mult = c(0.1, 0.1))) + theme + 
+  xlab("Signal strength") + ylab(" ") + 
+  guides(color = guide_legend(override.aes = list(size = 5)))
+
+
+
+### Supp Figure 6
+
+supp.fig.6 <-  
+  ggplot(data = fig.table %>% filter(rho == 0.3,
+                                     n == 500,
+                                     Method %in% c("ols-ada lasso naive CV",
+                                                   "ols-ada lasso nested CV",
+                                                   "lasso CV")),
+         mapping = aes(signal, Value, color = Method, group = Method)) +
+  stat_summary(fun = "mean", geom = "line", size = 0.8, linetype = 1) +
+  stat_summary(fun = "mean", geom = "point", size = 2) +
+  stat_summary(fun = "Inf_95", geom = "line", alpha = 0.7, linetype = 2,
+               size = 0.3) +
+  stat_summary(fun = "Sup_95",geom = "line", alpha =  0.7, linetype = 2,
+               size = 0.3) +
+  facet_grid( Criterion ~ p, scales = "free",
+              labeller = labeller(Criterion = label_value, p = label_both)) +
+  scale_color_manual(values = col) +
+  scale_y_continuous(limits = function (b) {
+    return(c(ifelse(min(b) == 1, 0.9, min(b)), max(b)))
+  }) +
+  scale_x_continuous(breaks = signal.seq,
+                     expand = expansion(mult = c(0.1, 0.1))) + theme + 
+  xlab("Signal strength") + ylab(" ") + 
+  guides(color = guide_legend(override.aes = list(size = 5)))
+
+
+# save Figures
+
+if ( ! file.exists("figures/") ) {
+  dir.create("figures")
+}
+
+ggsave(filename = "figures/main_fig_2.pdf", plot = main.fig.2, 
+       width = 8.5, height = 6, dpi = 600 , limitsize = FALSE, units = "in")
+
+
+
+
+ggsave(filename = "figures/supp_fig_1.pdf", plot = supp.fig.1, 
+       width = 8.5, height = 9, dpi = 600 , limitsize = FALSE, units = "in")
+
+ggsave(filename = "figures/supp_fig_2.pdf", plot = supp.fig.2, 
+       width = 8.5, height = 9, dpi = 600 , limitsize = FALSE, units = "in")
+
+ggsave(filename = "figures/supp_fig_3.pdf", plot = supp.fig.3, 
+       width = 8.5, height = 9, dpi = 600 , limitsize = FALSE, units = "in")
+
+ggsave(filename = "figures/supp_fig_4.pdf", plot = supp.fig.4, 
+       width = 8.5, height = 9, dpi = 600 , limitsize = FALSE, units = "in")
+
+ggsave(filename = "figures/supp_fig_5.pdf", plot = supp.fig.5, 
+       width = 8.5, height = 6, dpi = 600 , limitsize = FALSE, units = "in")
+
+ggsave(filename = "figures/supp_fig_6.pdf", plot = supp.fig.6, 
+       width = 8.5, height = 6, dpi = 600 , limitsize = FALSE, units = "in")
+
+
+
 
